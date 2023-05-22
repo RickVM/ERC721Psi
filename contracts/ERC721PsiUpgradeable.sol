1ca989e797325da5a719b70ad1f4ff6f66bf5382
RickVM <vanmelisrick@gmail.com>
2023-05-22 16:16:53 +0200

    feat: add storage gaps to prevent future collisions

--- contracts/ERC721PsiUpgradeable.sol
+++ contracts/ERC721PsiUpgradeable.sol
@@ -573,4 +573,9 @@ contract ERC721PsiUpgradeable is
     uint256 startTokenId,
     uint256 quantity
   ) internal virtual {}
+
+    /// @dev This empty reserved space is put in place to allow future versions to add new
+  /// variables without shifting down storage in the inheritance chain.
+  /// See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
+  uint256[43] private __gap;
 }
